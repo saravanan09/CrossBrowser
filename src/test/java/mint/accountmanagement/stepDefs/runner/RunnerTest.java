0116560c40f272f913f41cfab48cad6f4ea10e19
saravanan09 <vsaravanan1991@gmail.com>
2016-10-21 19:53:29 +0530

    Update RunnerTest.java

--- src/test/java/mint/accountmanagement/stepDefs/runner/RunnerTest.java
+++ src/test/java/mint/accountmanagement/stepDefs/runner/RunnerTest.java
@@ -10,7 +10,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "classpath:features",
-		plugin = {"pretty", "html:target/cucumber-html-report,json:target/cucumber.json",
+		plugin = {"pretty", "html:target/cucumber-html-report","json:target/cucumber.json",
                 "junit:target/cucumber.xml"},
 		tags = {"@ValidLogin,@InvalidLogin"}
 		)
